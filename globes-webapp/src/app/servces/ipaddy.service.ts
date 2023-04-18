ced41a9ab5c32a6cb6e891888663d7341fa12943
joeren2010 <sksports2002@gmail.com>
2023-04-18 15:01:22 -0600

    updated ec2 ip-addy in angular

--- globes-webapp/src/app/servces/ipaddy.service.ts
+++ globes-webapp/src/app/servces/ipaddy.service.ts
@@ -9,7 +9,7 @@ export class IpaddyService {
 
   public getIpAddy() {
     // return 'localhost'; //public ipv4 addy from aws' ec2 instance    
-    return '35.173.200.126'; //public ipv4 addy from aws' ec2 instance
+    return '172.31.90.36'; //public ipv4 addy from aws' ec2 instance
   }
 
 }
